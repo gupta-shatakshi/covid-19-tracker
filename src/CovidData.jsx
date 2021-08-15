f2e98f4dd97aa953ed26fddaca05af4a44ec7135
gupta-shatakshi <85827044+gupta-shatakshi@users.noreply.github.com>
2021-08-16 00:13:37 +0530

    Update CovidData.jsx

--- src/CovidData.jsx
+++ src/CovidData.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+// import axios from "axios";
 
 
 const CovidData = () => {
@@ -9,11 +9,14 @@ const CovidData = () => {
   const getData = async () => {
     try{
         // let res = await fetch("https://api.covid19india.org/data.json");
+        let res = await fetch("https://corona.lmao.ninja/v2/countries?yesterday&sort");
+        
         // let res = await axios.get("https://api.covid19india.org/data.json");
-        let res = await axios.get("https://cors-anywhere.herokuapp.com/https://api.covid19india.org/data.json");
+        //let res = await axios.get("https://corona.lmao.ninja/v2/countries?yesterday&sort");
+        //let res = await axios.get("https://cors-anywhere.herokuapp.com/https://api.covid19india.org/data.json");
         res = await res.json();
-        console.log(res);
-        setData(res.statewise[0]);
+        console.log(res[93]);
+        setData(res[93]);
         //console.log(res.statewise);
         //console.log(res.statewise[0].active);
     } catch (err){
@@ -42,12 +45,32 @@ const CovidData = () => {
             </li>
         </ul>
         </div>
+        <div className="main-card" style={{backgroundColor: "rgb(45, 48, 192)"}}>
+        <ul>
+            <li className="list">
+                <div className="card">
+                    <p className="heading"><span>{`TOTAL `}</span>TESTS</p>
+                    <p className="data">{data.tests}</p>
+                </div>
+            </li>
+        </ul>
+        </div>
         <div className="main-card" style={{backgroundColor: "rgb(56, 199, 204)"}}>
         <ul>
             <li className="list">
                 <div className="card">
-                    <p className="heading"><span>{`TOTAL `}</span>CONFIRMED</p>
-                    <p className="data">{data.confirmed}</p>
+                    <p className="heading"><span>{`TOTAL `}</span>CASES</p>
+                    <p className="data">{data.cases}</p>
+                </div>
+            </li>
+        </ul>
+        </div>
+        <div className="main-card" style={{backgroundColor: "rgb(193, 80, 228)"}}>
+        <ul>
+            <li className="list">
+                <div className="card">
+                    <p className="heading"><span>{`TOTAL `}</span>ACTIVE</p>
+                    <p className="data">{data.active}</p>
                 </div>
             </li>
         </ul>
@@ -66,36 +89,17 @@ const CovidData = () => {
         <ul>
             <li className="list">
                 <div className="card">
-                    <p className="heading"><span>{`TOTAL `}</span>DEATH</p>
+                    <p className="heading"><span>{`TOTAL `}</span>DEATHS</p>
                     <p className="data">{data.deaths}</p>
                 </div>
             </li>
         </ul>
         </div>
-        <div className="main-card" style={{backgroundColor: "rgb(193, 80, 228)"}}>
-        <ul>
-            <li className="list">
-                <div className="card">
-                    <p className="heading"><span>{`TOTAL `}</span>ACTIVE</p>
-                    <p className="data">{data.active}</p>
-                </div>
-            </li>
-        </ul>
-        </div>
-        <div className="main-card" style={{backgroundColor: "rgb(45, 48, 192)"}}>
-        <ul>
-            <li className="list">
-                <div className="card">
-                    <p className="heading"><span>{`LAST `}</span>UPDATED</p>
-                    <p className="data">{data.lastupdatedtime}</p>
-                </div>
-            </li>
-        </ul>
-        </div>
+        
+        
     </div>
     </>
   );
 }
 
 export default CovidData;
-
